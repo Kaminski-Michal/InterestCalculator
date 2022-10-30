c30c7a787972ff584a5f8f46edd742f1c4d9c7a7
Michał Kamiński <240421@edu.p.lodz.pl>
2022-10-30 13:50:11 +0100

    Income tax value taken from user

--- src/App.java
+++ src/App.java
@@ -1,19 +1,22 @@
 import javax.swing.JOptionPane;
 
+
 public class App {
     public static void main(String[] args) throws Exception {
       
-        String intrestRate = JOptionPane.showInputDialog("Give '%' of intrest (as Intiger number)");
+        int intrestRate = Integer.parseInt(JOptionPane.showInputDialog("Give '%'- per year of intrest (as Intiger number)"));
        
         int valueRegularly = Integer.parseInt(JOptionPane.showInputDialog("Give value you transfer every month"));
+
+        int taxValue = Integer.parseInt(JOptionPane.showInputDialog("What is income tax?\nIn Poland intrest tax is 19%"));
     
         String intrestDuration = JOptionPane.showInputDialog("For how long you deposit? (in months)\nif in years write y before value \nlike y2 - 2 years"); 
         
+
         ConvertYearsToMonths convert = new ConvertYearsToMonths(intrestDuration);
         int Duration =  convert.ReturnConvertedValue();
 
-        JOptionPane.showMessageDialog(null, "You gonna pay: " +valueRegularly+" every month\nfor "+intrestRate+"%\nfor next "+Duration+" months");
+        JOptionPane.showMessageDialog(null, "You gonna pay: " +valueRegularly+"zl every month\nfor "+intrestRate+"%\nfor next "+Duration+" months\n"+"Tax is: "+taxValue+"% from income");
 
-        System.out.println("DA");
     }
 }
