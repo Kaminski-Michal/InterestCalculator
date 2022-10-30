ad13189d7ecda576d722c9b7a3f0121cbe9796b2
Michał Kamiński <240421@edu.p.lodz.pl>
2022-10-30 17:07:19 +0100

    interest/interestRise changed for double added expire time for 1 bound import for Arrays

--- src/App.java
+++ src/App.java
@@ -4,24 +4,22 @@ import javax.swing.JOptionPane;
 public class App {
     public static void main(String[] args) throws Exception {
       
-        int intrestRate = Integer.parseInt(JOptionPane.showInputDialog("Give '%'- per year of intrest (as Intiger number)"));
-
-        int interestRise = Integer.parseInt(JOptionPane.showInputDialog("what is interest rise every year ('%' as Intiger number)"));
-       
+        double intrestRate = Double.parseDouble(JOptionPane.showInputDialog("Give '%'- per year of intrest (as Double number)"));
+        double interestRise = Double.parseDouble(JOptionPane.showInputDialog("what is interest rise every year ('%' as Double number)"));
         int valueRegularly = Integer.parseInt(JOptionPane.showInputDialog("Give value you transfer every month"));
-
         int taxValue = Integer.parseInt(JOptionPane.showInputDialog("What is capital tax?\nIn Poland tax is 19%"));
-    
-        String intrestDuration = JOptionPane.showInputDialog("For how long you deposit? (in months)\nif in years write y before/after value \nlike 2y - 2 years"); 
-        
+        String expireTime = JOptionPane.showInputDialog("What is expire time for 1 Bound? (in months)\nif in years write y before/after value \nlike 1y - 1 year");
 
+        ConvertYearsToMonths convertExpireTime = new ConvertYearsToMonths(expireTime);
+        int timeToExpire = convertExpireTime.ReturnConvertedValue();
+
+
+        String intrestDuration = JOptionPane.showInputDialog("For how long you plan to invest? (in months)\nif in years write y before/after value \nlike 2y - 2 years"); 
+        
         ConvertYearsToMonths convert = new ConvertYearsToMonths(intrestDuration);
         int Duration =  convert.ReturnConvertedValue();
 
-       
-        
+        Bank bank = new Bank(valueRegularly,intrestRate,Duration,taxValue,interestRise, timeToExpire);
 
-        Bank transferValues = new Bank(valueRegularly,intrestRate,Duration,taxValue,interestRise);
-        
     }
 }
